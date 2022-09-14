19f2d21d3b90c10df2673f9aa765419a3918393e
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-09-14 10:59:52 +0800

    Add title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from "next/head";
 import "../sass/app.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>WebGL iPhone 14 Pro Max</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
